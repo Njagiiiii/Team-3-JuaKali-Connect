88123d24bdec55070f1be95b7305a39cfa7b9e1a
Njagiiiii <rmknjagi@gmial.com>
2026-09-09 01:20:24 +0300

    Made changes to index.js

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
-   const express = require('express');
-   const cors = require('cors');
-   require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-   const app = express();
-   const PORT = process.env.PORT || 5000;
+const bookingRoutes = require("./routes/bookings");
 
-   app.use(cors());
-   app.use(express.json());
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-   app.get('/', (req, res) => {
-     res.send('API is running');
-   });
+app.use(cors());
+app.use(express.json());
 
-   const bookingRoutes = require('./routes/bookings');
-   app.use('/api/bookings', bookingRoutes);
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
-   app.listen(PORT, () => {
-     console.log(`Server running on http://localhost:${PORT}`);
-   });
+app.use("/api/bookings", bookingRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
